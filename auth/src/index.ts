0604e123114504eed4d39f738bc7e27184bf13e6
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-11-17 13:52:03 +0200

    normalizing errors responses - first attempt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,18 +1,19 @@
-import express from 'express'
-import {currentUserRouter} from './routes/current-user' 
-import {signinRouter} from './routes/signin' 
-import {signoutRouter} from './routes//signout' 
-import {signupRouter} from './routes/signup' 
-
-const app=express()
-app.use(express.json())
+import express from "express";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes//signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+const app = express();
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(currentUserRouter)
-app.use(signupRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(errorHandler);
 
-app.listen(3000,()=>{
-  console.log('Auth service listen on port 3000')
-})
+app.listen(3000, () => {
+  console.log("Auth service listen on port 3000");
+});
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { RequestValidationError } from "../errors/request-validation-error";
 const router = express.Router();
 
 router.post(
@@ -14,10 +16,15 @@ router.post(
   (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      //javascrip solution
+      // const error = new Error("Invalid email or password");
+      // error.reasons = errors.array();
+
+      throw new RequestValidationError(errors.array());
     }
     const { email, password } = req.body;
     //create user
+    throw new DatabaseConnectionError();
     res.send({ message: "Hi from signup!" });
   }
 );
