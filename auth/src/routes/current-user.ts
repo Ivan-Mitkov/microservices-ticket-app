a1b9ea9c8c0875372562f63b4c6b3911253b49f4
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-11-20 17:04:03 +0200

    get current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,24 @@
-import express from 'express'
-const router=express.Router();
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
+import { Password } from "../services/password";
 
+const router = express.Router();
 
+router.get("/api/users/currentuser", (req, res) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+  //check if jwt is valid
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.jwt!);
+    return res.send({ currentUser: payload });
+  } catch (error) {
+    return res.send({ currentUser: null });
+  }
+});
 
-router.get('/api/users/currentuser',(req,res)=>{
-res.send('Hi from curent user! To see response in chrome , click on browser and type thisisunsafe')
-})
-
-export {router as currentUserRouter}
+export { router as currentUserRouter };
