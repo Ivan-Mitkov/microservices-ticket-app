2f520b78f62bcced9cc2926cb1696b41da7b5643
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-11-21 12:59:47 +0200

    test environment setup

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,28 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+
+let mongo: any;
+//before test start create new instance of memory server
+beforeAll(async () => {
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  //reset all data
+  for (const collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+//stop memory server
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
