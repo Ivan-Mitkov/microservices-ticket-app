27347741f35aaabd10a16baa291f213b803b372a
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-11-21 15:45:37 +0200

    get auth cookie with global test function

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,12 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("responds with details about the current user ", async () => {
-  const authResponse = await request(app)
-    .post("/api/users/signup")
-    .send({ email: "test@test.com", password: "123456" })
-    .expect(201);
-  // console.log(authResponse.get("Set-Cookie"));
-  const cookie = authResponse.get("Set-Cookie");
+  const cookie = await global.signin();
   const response = await request(app)
     .get("/api/users/currentuser")
     .set("Cookie", cookie)
@@ -15,4 +10,11 @@ it("responds with details about the current user ", async () => {
     .expect(200);
   // console.log(response.body);
   expect(response.body.currentUser.email).toEqual("test@test.com");
+});
+it("responds with null if not authenticated ", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+  expect(response.body.currentUser).toEqual(null);
 });
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,7 +1,15 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
 
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 let mongo: any;
 //before test start create new instance of memory server
 beforeAll(async () => {
@@ -27,3 +35,18 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+//Define global function for getting authenticated user
+//works only in tests
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "123456";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+  return cookie;
+};
