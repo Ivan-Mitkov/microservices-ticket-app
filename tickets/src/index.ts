f7069e34b3013d0062cb4938525d9384e64f0a89
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-11-23 14:08:08 +0200

    use env for mongo uri

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,13 +6,16 @@ const start = async () => {
   if (!process.env.jwt) {
     throw new Error("ENV Variable Not Found");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("ENV Variable Not Found");
+  }
   //connect to ClusterIp service
   // name: auth-mongo-srv
   // ports:
   //   port: 27017
   try {
     //mongodb://[service]:[port]/[name of db mongo wiil create if not existing ]
-    await mongoose.connect("mongodb://tickets-mongo-srv:27017/tickets", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
