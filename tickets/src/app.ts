031e751e174c6df38c482cab9c8f6fee04f5b5d8
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-11-24 14:39:37 +0200

    get tickets, also make minor change in common

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,6 +3,7 @@ import "express-async-errors";
 import cookieSession from "cookie-session";
 import { currentUser, errorHandler, NotFoundError } from "@microauth/common";
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
 const app = express();
 //traffic comes from ingress proxy so we tell express to trust this proxy
 app.set("trust proxy", true);
@@ -14,8 +15,9 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
-app.use(currentUser)
+app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
