aa215bbcaba2ab6d7589d21daab27f9557b362a9
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-11-30 18:27:29 +0200

    delete order route and test

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,60 @@
+import { OrderStatus } from "@microauth/common";
+import request from "supertest";
+import { app } from "../../app";
+import Order from "../../models/Orders";
+import Ticket from "../../models/Ticket";
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: "football",
+    price: 100,
+  });
+  await ticket.save();
+  return ticket;
+};
+it("should cancell the order", async () => {
+  //create a tickets
+  const ticketOne = await buildTicket();
+
+  //create  users
+  const user = global.signin();
+  //create one order
+  const orderResponse = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+  // console.log(orderResponse);
+  //Make request to cancel order for user
+  await request(app)
+    .delete(`/api/orders/${orderResponse.body.order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  const updatedOrder = await Order.findById(orderResponse.body.order.id);
+  expect(updatedOrder?.status).toEqual(OrderStatus.Cancelled);
+});
+
+it("should not cancell other user order", async () => {
+  //create a tickets
+  const ticketOne = await buildTicket();
+
+  //create  users
+  const user = global.signin();
+  const user2 = global.signin();
+  //create one order
+  const orderResponse = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+
+  //Make request to cancel order for user
+  await request(app)
+    .delete(`/api/orders/${orderResponse.body.order.id}`)
+    .set("Cookie", user2)
+    .expect(401);
+});
+
+it.todo("emit event when cancelled ");
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,15 +1,30 @@
 import express, { Request, Response } from "express";
-import { NotFoundError } from "@microauth/common";
+import {
+  NotFoundError,
+  requireAuth,
+  NotAuthorizedError,
+} from "@microauth/common";
 
-import Order from "../models/Orders";
+import Order, { OrderStatus } from "../models/Orders";
 
 const router = express.Router();
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  const response = await Order.findByIdAndRemove({ id: req.params.orderId });
-  if (!response) {
-    throw new NotFoundError();
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId);
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    //update status
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    //@TODO publish an event that order is cancelled
+
+    res.status(204).send(order);
   }
-  // console.log(response);
-  res.status(200).send(response);
-});
+);
 export { router as deleteOrderRouter };
