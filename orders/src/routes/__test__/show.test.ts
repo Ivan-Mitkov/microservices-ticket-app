4a1a5a28faff752f6fb7e7aae049891e6671f941
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-11-30 18:03:18 +0200

    show rote and tests

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,61 @@
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+import Order, { OrderStatus } from "../../models/Orders";
+import Ticket from "../../models/Ticket";
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: "football",
+    price: 100,
+  });
+  await ticket.save();
+  return ticket;
+};
+it("should fetches the order", async () => {
+  //create a tickets
+  const ticketOne = await buildTicket();
+
+  //create  users
+  const user = global.signin();
+  //create one order
+  const orderResponse = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+  // console.log(orderResponse);
+  //Make request to get order for user
+  const response = await request(app)
+    .get(`/api/orders/${orderResponse.body.order.id}`)
+    .set("Cookie", user)
+    .expect(200);
+
+  // console.log(response.body);
+  // console.log(orderResponse.body);
+
+  expect(response.body.id).toEqual(orderResponse.body.order.id);
+
+  expect(response.body.ticket.id).toEqual(orderResponse.body.order.ticket.id);
+});
+
+it("should not fetch other user order", async () => {
+  //create a tickets
+  const ticketOne = await buildTicket();
+
+  //create  users
+  const user = global.signin();
+  const user2 = global.signin();
+  //create one order
+  const orderResponse = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+
+  //Make request to get order for user
+  await request(app)
+    .get(`/api/orders/${orderResponse.body.order.id}`)
+    .set("Cookie", user2)
+    .expect(401);
+});
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,15 +1,28 @@
 import express, { Request, Response } from "express";
-import { NotFoundError } from "@microauth/common";
+import {
+  NotFoundError,
+  requireAuth,
+  NotAuthorizedError,
+} from "@microauth/common";
 
 import Order from "../models/Orders";
 
 const router = express.Router();
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  const response = await Order.find({ id: req.params.orderId });
-  if (!response) {
-    throw new NotFoundError();
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const response = await Order.findById(req.params.orderId).populate(
+      "ticket"
+    );
+    if (!response) {
+      throw new NotFoundError();
+    }
+    if (response.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    // console.log(response);
+    res.status(200).send(response);
   }
-  // console.log(response);
-  res.status(200).send(response);
-});
+);
 export { router as showOrderRouter };
