162a86d0a064da8b73032f29cd03fd1edf8ad5bf
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-12-11 14:20:44 +0200

    payments - create payment routes, ingress

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import "express-async-errors";
 import cookieSession from "cookie-session";
 import { currentUser, errorHandler, NotFoundError } from "@microauth/common";
-
+import { createChargeRouter } from "./routes/new";
 const app = express();
 //traffic comes from ingress proxy so we tell express to trust this proxy
 app.set("trust proxy", true);
@@ -15,6 +15,7 @@ app.use(
   })
 );
 app.use(currentUser);
+app.use(createChargeRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
